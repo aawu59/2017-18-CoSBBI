b105ce51059e03c0781cdecccb6975a937b76a11
aawu59 <30479218+aawu59@users.noreply.github.com>
2018-08-03 17:51:04 -0400

    Update DataTest.py

--- DataTest.py
+++ DataTest.py
@@ -3,15 +3,13 @@ import DataAnalysis
 DataAnalysis.point_cutoffs()
 DataAnalysis.data_processing()
 
-# try:
-#     DataAnalysis.firstplot()
-#     # Displays plot in Matplotlib and saves png file to temp.png
-# except:
-#     pass
+try:
+    DataAnalysis.firstplot()
+    # Displays plot in Matplotlib and saves png file to temp.png
+except:
+    pass
 
-# try:
-#     DataAnalysis.extrapoints()
-# except:
-#     pass
-
-DataAnalysis.slices()
+try:
+    DataAnalysis.extrapoints()
+except:
+    pass
